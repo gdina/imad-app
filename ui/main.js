12a152be1d1363c0fafff365c5853a3812e53b2d
gdina <30625782+gdina@users.noreply.github.com>
2017-08-12 19:19:53 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,9 @@
+var button = document.getElementById('click_likes');
+var counter = 0 ;
+button.onClick = function () {
+    
+    counter = counter + 1 ;
+    var span = document.getElementById('likes');
+    span.innerHTML = counter.toString() ;
+    
+}
