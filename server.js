d14943a2cccd07a728f3efc9d333d37fd8205391
gdina <30625782+gdina@users.noreply.github.com>
2017-08-12 12:20:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,11 @@ app.get('/:pageName', function (req, res) {
   res.send(createTemplate(pages[pageName]));
 });
 
+app.get('/counters', function (req, res){
+    counter = counter + 1;
+    //res.send(counter,toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
