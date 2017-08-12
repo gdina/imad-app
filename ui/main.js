4733eb3de472bca341c3c063c44a710b137e6de5
gdina <30625782+gdina@users.noreply.github.com>
2017-08-12 22:00:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,26 @@ var button = document.getElementById('click_likes');
 
 var counter = 0 ;
 
-button.onclick = function ( ) {
+button.onclick = function ( ) 
+{
     
-    counter = counter + 1 ;
-    var span = document.getElementById('likes');
-    span.innerHTML = counter.toString() ;
+    var request =  new XMLHttpRequest() ;
     
+    request.onreadystatechange = function() 
+    {
+        
+        if (request.readyState === XMLHttpRequest.DONE )
+        {
+             if (request.status === 200 )
+             {
+                    var counter = request.responseText;
+                    var span = document.getElementById('likes');
+                    span.innerHTML = counter.toString() ;
+             }
+        }
+    
+    };
+    
+     request.open('GET','http://gdinakar96.imad.hasura-app.io/counters');
+     request.send(null);
 };
