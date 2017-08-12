3eda74d16ebddfdfdddc1be2c92997bed234379a
gdina <30625782+gdina@users.noreply.github.com>
2017-08-12 12:42:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,8 +84,9 @@ app.get('/:pageName', function (req, res) {
   if ( pageName == 'counters' )
   {
       counter = counter + 1 ;
-     // res.send(counter,toString());
-     alert('value in counter is' + counter.toString());
+      res.send(counter.toString());
+      
+     //alert('value in counter is' + counter.toString());
        //res.sendFile(path.join(__dirname,'ui','qualification.html'));
   }
      else 
